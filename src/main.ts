a66e89f6b767ae38c9a9c1acec50c3baf2128088
leasabus <leandro.sabus@gmail.com>
2025-08-07 18:53:51 -0300

    add enable cors to fetch data in frontend

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,17 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   const config = new DocumentBuilder()
     .setTitle('API INNOVACORE')
-    .setDescription('Documentación de la API para la gestion del sanatorio de rosario')
+    .setDescription(
+      'Documentación de la API para la gestion del sanatorio de rosario',
+    )
     .setVersion('1.0')
     .addBearerAuth() // Si usás JWT o auth
     .build();
@@ -15,7 +23,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
