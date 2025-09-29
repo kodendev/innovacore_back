4e3c2760fb0f4e3c69dfab3279784b5ad03bf490
leasabus <leandro.sabus@gmail.com>
2025-09-29 15:14:39 -0300

    add filter to show only active suppliers

--- src/suppliers/suppliers.service.ts
+++ src/suppliers/suppliers.service.ts
@@ -60,6 +60,7 @@ export class SuppliersService {
 
   async findAll(): Promise<Supplier[]> {
     return this.supplierRepo.find({
+      where: { active: true },
       relations: ['supplierProducts', 'supplierProducts.product'],
     });
   }
