2917ff7d2781830940d8f3e89a92b460d64b7b8c
Gaston <gmanzano12@gmail.com>
2025-11-07 23:39:57 -0300

    Adding post endpoint for categories.

--- src/inventory/controllers/categories.controller.ts
+++ src/inventory/controllers/categories.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { ApiOperation, ApiTags, ApiResponse } from '@nestjs/swagger';
 import { CategoryService } from '../services/category.service';
+import { CreateCategoryDto } from '../dto/create-category.dto';
 
 @ApiTags('Categorias de productos')
 @Controller('categories')
@@ -13,5 +14,19 @@ export class CategoriesController {
   })
   findAll() {
     return this.categoryService.findAll();
+  }
+
+  @Post()
+  @ApiOperation({ summary: 'Crear una nueva categoría' })
+  @ApiResponse({
+    status: 201,
+    description: 'La categoría fue creada correctamente',
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Datos inválidos en el cuerpo de la solicitud',
+  })
+  create(@Body() createCategoryDto: CreateCategoryDto) {
+    return this.categoryService.create(createCategoryDto);
   }
 }
